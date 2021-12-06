56631b7c9db0f7ac555f1a04468be96a5d99544f
Cameron Grieder <cjgrieder@gmail.com>
2021-12-06 07:46:16 -0700

    cleaning up code, removing unessisary empty lines

--- addzero.js
+++ addzero.js
@@ -4,12 +4,9 @@ let z = 0
 for (let x = 0;x < array.length; x++){
   for(let y = 0; y < array.length; y++){
     if(array[x] + array[y] == 0){
-      z++
-}
+      z++}
   }
 }
 if (z > 0){
-  console.log('true')
-}else {
-  console.log('false')
-}
+  console.log('true')}
+  else {console.log('false')}
